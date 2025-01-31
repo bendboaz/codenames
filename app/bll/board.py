e671cf8473ba598c2a536e201c0a097c4375bca7
Boaz Ben-Dov <bendboaz@gmail.com>
2025-01-31 16:08:46 +0200

    Add Board initializer

--- app/bll/board.py
+++ app/bll/board.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from dataclasses import Field
 
 from pydantic import BaseModel
@@ -9,10 +10,14 @@ from app.bll.types import AgentType, Coordinate
 class AgentPlacements(BaseModel):
     positions: dict[AgentType, list[Coordinate]]
     starting_color: AgentType
-    _shadow_board: list[list[AgentType]] = Field(default_factory=lambda: AgentPlacements.build_shadow_board_static())
+    _shadow_board: list[list[AgentType]] = Field(
+        default_factory=lambda: AgentPlacements.build_shadow_board_static()
+    )
 
     @staticmethod
-    def build_shadow_board_static(positions: dict[AgentType, list[Coordinate]] = {}) -> list[list[AgentType]]:
+    def build_shadow_board_static(
+        positions: dict[AgentType, list[Coordinate]] = {},
+    ) -> list[list[AgentType]]:
         shadow_board = get_empty_board(empty_value=AgentType.INNOCENT)
         for agent, coordinates in positions.items():
             for coord in coordinates:
@@ -23,7 +28,22 @@ class AgentPlacements(BaseModel):
         return self._shadow_board[item.x][item.y]
 
 
-class Board(BaseModel):
+class Board:
     cards: list[list[str]]
     discovered_agents: list[Coordinate]
     agent_placements: AgentPlacements
+
+    def __init__(
+        self,
+        words: list[list[str]],
+        agent_placements: AgentPlacements,
+        discovered_agents: list[Coordinate] = [],
+    ):
+        super().__init__()
+
+        if any(len(row) != len(words) for row in words):
+            raise ValueError("The words array must be a square array.")
+
+        self.words = deepcopy(words)
+        self.discovered_agents = discovered_agents[:]
+        self.agent_placements = agent_placements.model_copy()
